cc30df5352713ab441dec70bf023d4dc4f73aacf
Arturo Mendoza <jarsmdza@gmail.com>
2026-03-05 02:03:18 -0600

    Se agregaron mas funcionalidades al proyecto, la posibilidad de crear alerta, calcular precio, mostrar el spinner, agregar informacion a la tabla y leer los eventos de manera correcta

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,6 +4,28 @@ function Seguro(marca, year, tipo) {
     this.tipo = tipo;
 }
 
+Seguro.prototype.calcularPrecio = function() {
+    const tablaMarca = {
+        americano: 1.15,
+        asiatico: 1.05,
+        europeo: 1.35
+    };
+
+    const tablaTipo = {
+        basico: 30,
+        completo: 50
+    };
+
+    const yearDiferencia = ((new Date().getFullYear() - this.year) * 3)/100;
+    const base = 2000;
+    const costoMarca = base * tablaMarca[this.marca];
+    const costoTipo = costoMarca + (costoMarca* (tablaTipo[this.tipo] / 100));
+    const costoYear = costoTipo - (costoTipo * yearDiferencia);
+
+    const costoFinal = costoYear;
+    return costoFinal;
+}
+
 function UI() {}
 const ui = new UI();
 
@@ -15,10 +37,86 @@ UI.prototype.agregarDatosYear = () =>{
     for (let i=0; i<max; i++) {
         const option = document.createElement(`option`);
         option.textContent = year - i;
-        option.value = year - 1;
+        option.value = year - i;
         optionYear.appendChild(option);
     }
 };
+
+UI.prototype.agregarAlerta = function(texto, tipo) {
+    const existe = document.querySelector('.alert');
+    if(existe) {
+        existe.remove();
+    };
+    const info = document.querySelector('.info');
+    const etiquetaP = document.createElement('P');
+    etiquetaP.textContent = texto;
+    etiquetaP.classList.add(`alert`, `${tipo}`);
+    info.appendChild(etiquetaP);
+    setTimeout(()=>{
+        etiquetaP.remove();
+    }, 3000);
+};
+
+UI.prototype.mostrarSpinner = () => {
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'block';
+    setTimeout(()=>{
+        spinner.style.display = 'none';
+    }, 3000);
+}
+
+UI.prototype.agregarInfoTabla = (total, seguro) => {
+    const table = document.querySelector('.table');
+    const tableBody = document.querySelector('.tbody');
+
+    const filMarca = document.createElement('tr')
+    const colMarca = document.createElement('td')
+    colMarca.textContent = seguro.marca;
+    filMarca.appendChild(colMarca);
+    tableBody.appendChild(filMarca);
+
+    const filYear = document.createElement('tr')
+    const colYear = document.createElement('td')
+    colYear.textContent = seguro.year;
+    filYear.appendChild(colYear);
+    tableBody.appendChild(filYear);
+
+    const filTipo = document.createElement('tr')
+    const colTipo = document.createElement('td')
+    colTipo.textContent = seguro.tipo;
+    filTipo.appendChild(colTipo);
+    tableBody.appendChild(filTipo);
+
+    const filTotal = document.createElement('tr')
+    const colTotal = document.createElement('td')
+    colTotal.textContent = total;
+    filTotal.appendChild(colTotal);
+    tableBody.appendChild(filTotal);
+
+}
+
 document.addEventListener('DOMContentLoaded', () =>{
     ui.agregarDatosYear();
+    leerEventos();
 });
+
+function leerEventos() {
+    const form = document.querySelector('.form');
+    const marca = document.querySelector('#marca');
+    const year = document.querySelector('#year');
+    form.addEventListener('submit', e => {
+        const tipo = document.querySelector('input[name="tipo"]:checked');
+        e.preventDefault();
+        if (marca.value === '' || year.value === '' || tipo.value === '') {
+            ui.agregarAlerta('Todos los campos son obligatorios.', 'incorrecto');
+            return;
+        }
+        ui.agregarAlerta('Cotizando...', 'correcto');
+        ui.mostrarSpinner();
+        const seguro = new Seguro(marca.value, year.value, tipo.value);
+        const precioTotal = seguro.calcularPrecio();
+        
+        const table = document.querySelector('.table').style.display = 'table';
+        ui.agregarInfoTabla(precioTotal, seguro);
+    });
+}
